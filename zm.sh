e933b50fc61b14959eeaee0ce7534ee85d10d1a1
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-28 21:29:15 -0400

    Create zm.sh

---
+++ zm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+### In zm.sh (make sure this file is chmod +x):
+# `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
+# If you omit that part, the command will be run as root.
+
+exec /usr/bin/zm >>/var/log/zm.log 2>&1
