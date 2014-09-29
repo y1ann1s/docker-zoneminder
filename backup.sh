0113998055b74c8c4df6d626210e7dfcc8af4b99
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-09-28 20:49:29 -0400

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Backup mysql
+mysqldump -u root -pmysqlpsswd --all-databases > /var/backups/alldb_backup.sql
+
+#Backup important file ... of the configuration ...
+cp  /etc/hosts  /var/backups/
+
+#Backup importand files relate to app
+#cp /var/www/owncloud/config/config.php /var/backups/
