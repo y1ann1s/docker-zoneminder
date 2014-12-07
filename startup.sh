12d5a79d7c7cc40005443609cbf76a176cbdf921
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-06 22:11:01 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,15 +6,6 @@ set -e
 if [ -f /etc/configured ]; then
         echo 'already configured'
 else
-        #openssl req -new -x509 -days 365 -nodes -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=$CN"  -out /etc/apache2/ssl/owncloud.pem -keyout /etc/apache2/ssl/owncloud.key
-        #sed -i 's/ServerName example.com/ServerName $CN/' /etc/apache2/conf.d/owncloud.conf
-        #to include the domain to the hosts
-        #echo 127.0.0.1 $CN >> /etc/hosts
-        # for security change permission of folder and files
-        #chown -R www-data:www-data /var/www/owncloud
-        #find /var/www/owncloud -type d -exec chmod 750 {} \;
-        #find /var/www/owncloud -type f -exec chmod 640 {} \;
-        
         #configuration for zoneminder 
         chown -R www-data:www-data /var/cache/zoneminder/events
         chown -R www-data:www-data /var/cache/zoneminder/images
