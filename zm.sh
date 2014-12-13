77873a6edd4287d88d100c601a6d29e9b0dc7cf9
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-13 02:26:31 -0500

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -3,4 +3,6 @@
 # `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
 # If you omit that part, the command will be run as root.
 
+sleep 15
+
 exec /usr/bin/zmpkg.pl start >>/var/log/zm.log 2>&1
