f9afae5eedad95bf9f8bf001c4de9e7a9809c43c
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-29 23:41:45 -0500

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -2,7 +2,14 @@
 ### In zm.sh (make sure this file is chmod +x):
 # `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
 # If you omit that part, the command will be run as root.
+ZM_PATH_BIN="/usr/bin"
+RUNDIR=/var/run/zm
+TMPDIR=/tmp/zm
+command="$ZM_PATH_BIN/zmpkg.pl"
 
 sleep 10
 
-exec /usr/bin/zmpkg.pl start >>/var/log/zm.log 2>&1
+mkdir -p $RUNDIR && chown www-data:www-data $RUNDIR
+mkdir -p $TMPDIR && chown www-data:www-data $TMPDIR
+
+exec $command start >>/var/log/zm.log 2>&1
