daa15adff25b275fae3f3b0f37112f1fb3baa6fc
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-30 21:28:39 -0500

    Update mysqld.sh

--- mysqld.sh
+++ mysqld.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ### In mysqld.sh (make sure this file is chmod +x):
 # `/sbin/setuser mysql` runs the given command as the user `mysql`.
 # If you omit that part, the command will be run as root.
