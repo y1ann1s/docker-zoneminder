9a41a3c9f670c3b77c5e6519f42f3dea7c75f49d
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-30 23:58:12 -0500

    Delete finish_mysqld.sh

--- finish_mysqld.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### In finish_mysqld.sh (make sure this file is chmod +x):
-# `/sbin/setuser mysql` runs the given command as the user `mysql`.
-# If you omit that part, the command will be run as root.
-
-/bin/kill -9 $(/bin/pidof mysqld)
-/bin/kill -9 $(/bin/pidof mysqld_safe)
-exit 0
