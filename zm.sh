dc64931dc30a73d6505028a7f26349f52527da73
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-30 23:56:47 -0500

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -3,15 +3,8 @@
 # `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
 # If you omit that part, the command will be run as root.
 
-ZM_PATH_BIN="/usr/bin"
-RUNDIR=/var/run/zm
-TMPDIR=/tmp/zm
-command="$ZM_PATH_BIN/zmpkg.pl"
 
 svwaitup 3 /etc/service/apache2 /etc/service/mysqld || exit 1
 
-mkdir -p $RUNDIR && chown www-data:www-data $RUNDIR
-mkdir -p $TMPDIR && chown www-data:www-data $TMPDIR
-
 exec 2>&1
-exec $command start --pid /var/run/zm/zm.pid >>/var/log/zm.log
+exec /etc/init.d/zoneminder start  >>/var/log/zm.log
