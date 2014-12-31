40696fbe59c3005e9713178a3df51fa9377f5341
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2014-12-31 00:00:47 -0500

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -4,7 +4,7 @@
 # If you omit that part, the command will be run as root.
 
 
-svwaitup 3 /etc/service/apache2 /etc/service/mysqld || exit 1
+#svwaitup 3 /etc/service/apache2 /etc/service/mysqld || exit 1
 
 exec 2>&1
 exec /etc/init.d/zoneminder start  >>/var/log/zm.log
