429f9958c27f1a9c01ba618f79e3214ac227e9c4
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-04-05 10:03:07 -0400

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,5 +6,3 @@ mysqldump -u root -pmysqlpsswd --all-databases > /var/backups/alldb_backup.sql
 #Backup important file ... of the configuration ...
 cp  /etc/hosts  /var/backups/
 
-#Backup importand files relate to app
-#cp /var/www/owncloud/config/config.php /var/backups/
