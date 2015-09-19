498e2fd7f69ffcc2a30e7d37a4673878f4b19633
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-09-19 09:34:41 -0400

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -11,6 +11,9 @@ apt-get update && apt-get install -y -q build-essential \
                               libxfixes-dev \
                               zlib1g-dev \
                               pkg-config \
+                              libx264-dev \
+                              libfaac-dev \
+                              libmp3lame-dev \
                               netcat 
                               
 FFMPEG_VERSION=2.7.2
@@ -23,7 +26,7 @@ FFMPEG_VERSION=2.7.2
  fi
 
  cd "ffmpeg-${FFMPEG_VERSION}"
- ./configure --enable-version3 --enable-postproc --enable-libvorbis --enable-libvpx
+ ./configure --enable-version3 --enable-postproc --enable-libmp3lame --enable-libvorbis --enable-libvpx --enable-gpl --enable-libx264 --enable-nonfree --enable-libfaac
  make
  checkinstall --pkgname=ffmpeg --pkgversion="5:${FFMPEG_VERSION}" --backup=no --deldoc=yes --default
 
