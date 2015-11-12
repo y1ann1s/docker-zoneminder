a41734e6cbdfcdcd774ec30db8668f783bf4cf59
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-11-11 19:19:10 -0500

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -16,7 +16,7 @@ apt-add-repository multiverse && apt-get update && apt-get install -y -q build-e
                               libmp3lame-dev \
                               netcat 
                               
-FFMPEG_VERSION=2.7.2
+FFMPEG_VERSION=2.8.1
 
  cd /usr/local/src
  if [ ! -d "/usr/local/src/ffmpeg-${FFMPEG_VERSION}" ]; then
