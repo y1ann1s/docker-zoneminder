749808bfef47880463b12736506b1cc674f25d18
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-02-10 13:40:40 -0500

    Update zm.sh
    relate to change at --shm-size command ...at docker run

--- zm.sh
+++ zm.sh
@@ -2,10 +2,6 @@
 ### In zm.sh (make sure this file is chmod +x):
 # `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
 # If you omit that part, the command will be run as root.
-
-umount /dev/shm
-mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size=${MEM:-4096M} tmpfs /dev/shm
-
 sleep 10s
 
 /etc/init.d/ntp start  >>/var/log/ntp.log 2>&1
