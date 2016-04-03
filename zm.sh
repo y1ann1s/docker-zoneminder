0d55ff456e026af1d48d022c7bae7adbacf27ca5
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-03 16:23:19 -0400

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ### In zm.sh (make sure this file is chmod +x):
-# `/sbin/setuser xxxx` runs the given command as the user `xxxx`.
+# `chpst -u root` runs the given command as the user `root`.
 # If you omit that part, the command will be run as root.
-sleep 10s
 
-/etc/init.d/ntp start  >>/var/log/ntp.log 2>&1
-/etc/init.d/zoneminder start  >>/var/log/zm.log 2>&1
+sv -w7 check apache2
+
+exec chpst -u root /etc/init.d/zoneminder start 2>&1
