1ea90e822c10ee23a28a26ca2ead1ce2ef544a50
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-03 16:13:46 -0400

    Update mysqld.sh

--- mysqld.sh
+++ mysqld.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 ### In mysqld.sh (make sure this file is chmod +x):
-# `/sbin/setuser mysql` runs the given command as the user `mysql`.
+# `chpst -u mysql` runs the given command as the user `mysql`.
 # If you omit that part, the command will be run as root.
 
-exec 2>&1
-exec /sbin/setuser mysql /usr/bin/mysqld_safe >>/var/log/mysqld.log 
+exec chpst -u mysql /usr/bin/mysqld_safe  2>&1 
