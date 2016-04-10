af3386e4f6e1bd1e69a10c5981a8a0a1edfe16e7
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-10 08:25:09 -0400

    Update zm.sh

--- zm.sh
+++ zm.sh
@@ -4,5 +4,5 @@
 # If you omit that part, the command will be run as root.
 
 sleep 7s
-
-exec chpst -u root /usr/bin/zmpkg.pl start 2>&1
+ 
+exec chpst -u root /usr/bin/zmpkg.pl start >>/var/log/zm/zm.log 2>&1
