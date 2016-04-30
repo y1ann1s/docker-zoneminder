348ab17543742993c8a5f9ae0d21ce5fbc98144a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2016-04-30 13:17:46 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -11,6 +11,28 @@ else
         #to fix problem with data.timezone that appear at 1.28.108 for some reason
         sed  -i 's/\;date.timezone =/date.timezone = \"America\/New_York\"/' /etc/php5/apache2/php.ini
         #configuration for zoneminder 
+        #this only happends if -V was used and data was not from another container for that reason need to recreate the db.
+        if [ ! -f /var/lib/mysql/ibdata1 ]; then
+          mysql_install_db
+          #create database for zm
+          /usr/bin/mysqld_safe &
+          sleep 7s
+          mysqladmin -u root password mysqlpsswd
+          mysqladmin -u root -pmysqlpsswd reload
+          mysqladmin -u root -pmysqlpsswd create zm
+          echo "grant select,insert,update,delete on zm.* to 'zmuser'@localhost identified by 'zmpass'; flush privileges; " | mysql -u root -pmysqlpsswd
+          mysql -u root -pmysqlpsswd < /usr/share/zoneminder/db/zm_create.sql
+          killall mysqld
+          sleep 5s
+        fi
+        
+        #check if Directory inside of /var/cache/zoneminder are present.
+        if [ ! -d /var/cache/zoneminder/events ]; then
+           mkdir -p /var/cache/zoneminder/events
+           mkdir -p /var/cache/zoneminder/images
+           mkdir -p /var/cache/zoneminder/temp
+        fi
+        
         chown -R root:www-data /var/cache/zoneminder /etc/zm/zm.conf
         chown -R mysql:mysql /var/lib/mysql 
         chmod -R 770 /var/cache/zoneminder /etc/zm/zm.conf
