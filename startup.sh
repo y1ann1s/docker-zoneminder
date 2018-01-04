a88968fe5a9a1ed863e2d87aaebbcecfde554124
Marcelo Ochoa <mochoa@temperies.com>
2018-01-04 16:56:58 -0300

    add db hostname in backup script and optimize startup.sh to not execute chown at cache folder whic took a long time with large datasets

--- startup.sh
+++ startup.sh
@@ -3,18 +3,21 @@
 
 set -e
 
-if [ -f /etc/configured ]; then
+#trays to fix problem with https://github.com/QuantumObject/docker-zoneminder/issues/22
+chown www-data /dev/shm
+mkdir -p /var/run/zm
+chown www-data:www-data /var/run/zm
+#to fix problem with data.timezone that appear at 1.28.108 for some reason
+sed  -i "s|\;date.timezone =|date.timezone = \"${TZ:-America/New_York}\"|" /etc/php/7.0/apache2/php.ini
+if [ -f /var/cache/zoneminder/configured ]; then
         echo 'already configured'
         
         /sbin/zm.sh&
 else
-        #to fix problem with data.timezone that appear at 1.28.108 for some reason
-        sed  -i "s|\;date.timezone =|date.timezone = \"${TZ:-America/New_York}\"|" /etc/php/7.0/apache2/php.ini
+        #wait until MySQL container start
         sleep 45s
         
         #configuration for zoneminder
-        #trays to fix problem with https://github.com/QuantumObject/docker-zoneminder/issues/22
-        chown www-data /dev/shm
         #cp /etc/mysql/mysql.conf.d/mysqld.cnf /usr/my.cnf
         #this only happends if -V was used and data was not from another container for that reason need to recreate the db.
         if [ ! -f /var/cache/zoneminder/dbcreated ]; then
@@ -36,7 +39,7 @@ else
         
         #needed to fix problem with ubuntu ... and cron 
         update-locale
-        date > /etc/configured
+        date > /var/cache/zoneminder/configured
         
         /sbin/zm.sh&
 fi
