0735e8d0b120a64cb82f1194557c706343c4b6e7
David Luther <DCandE@users.noreply.github.com>
2018-07-13 16:46:33 -0500

    Update startup.sh
    Add MYSQL_ROOT_PASSWORD env variable

--- startup.sh
+++ startup.sh
@@ -13,12 +13,15 @@ sed  -i "s|\;date.timezone =|date.timezone = \"${TZ:-America/New_York}\"|" /etc/
 #if ZM_DB_HOST variable is provided in container use it as is, if not left as localhost
 ZM_DB_HOST=${ZM_DB_HOST:-localhost}
 sed  -i "s|ZM_DB_HOST=localhost|ZM_DB_HOST=$ZM_DB_HOST|" /etc/zm/zm.conf
+#if MYSQL_ROOT_PASSWORD variable is provided in container use it as is, if not left as mysqlpsswd
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-mysqlpsswd}
+sed  -i "s|MYSQL_ROOT_PASSWORD=mysqlpsswd|MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD|" /etc/zm/zm.conf
 #if ZM_SERVER_HOST variable is provided in container use it as is, if not left 02-multiserver.conf unchanged
 if [ -v ZM_SERVER_HOST ]; then sed -i "s|#ZM_SERVER_HOST=|ZM_SERVER_HOST=${ZM_SERVER_HOST}|" /etc/zm/conf.d/02-multiserver.conf; fi
 
 # Returns true once mysql can connect.
 mysql_ready() {
-  mysqladmin ping --host=$ZM_DB_HOST --user=root --password=mysqlpsswd > /dev/null 2>&1
+  mysqladmin ping --host=$ZM_DB_HOST --user=root --password=$MYSQL_ROOT_PASSWORD > /dev/null 2>&1
 }
 
 if [ -f /var/cache/zoneminder/configured ]; then
@@ -40,8 +43,8 @@ else
         
         chown -R root:www-data /var/cache/zoneminder /etc/zm/zm.conf
         chmod -R 770 /var/cache/zoneminder /etc/zm/zm.conf
-        echo "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';" | mysql -u root -pmysqlpsswd -h $ZM_DB_HOST
-        mysql -u root -pmysqlpsswd -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql   
+        echo "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';" | mysql -u root -$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST
+        mysql -u root -$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql   
         date > /var/cache/zoneminder/dbcreated
         #needed to fix problem with ubuntu ... and cron 
         update-locale
