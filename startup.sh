023436dc3a85f05f17c64dcaffa6cb5f19ba5bfb
David Luther <DCandE@users.noreply.github.com>
2018-07-13 21:51:19 -0500

    Update startup.sh
    root not rootr

--- startup.sh
+++ startup.sh
@@ -44,7 +44,7 @@ else
         chown -R root:www-data /var/cache/zoneminder /etc/zm/zm.conf
         chmod -R 770 /var/cache/zoneminder /etc/zm/zm.conf
         echo "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION';" | mysql -u root -p$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST
-        mysql -u rootr -p$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql   
+        mysql -u root -p$MYSQL_ROOT_PASSWORD -h $ZM_DB_HOST < /usr/share/zoneminder/db/zm_create.sql   
         date > /var/cache/zoneminder/dbcreated
         #needed to fix problem with ubuntu ... and cron 
         update-locale
