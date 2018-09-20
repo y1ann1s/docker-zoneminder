cdaff821289c34c8746c7297fdb39703d6b00d5a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2018-09-20 01:03:30 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -7,7 +7,8 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y -q php-gd zoneminder
 echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
 ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
 
-cp /zoneminder.conf /etc/apache2/conf-available/zoneminder.conf
+#checking if zoneminder.conf change with the new version .... at fix_issue 
+#cp /zoneminder.conf /etc/apache2/conf-available/zoneminder.conf
 
 a2enmod cgi
 a2enconf zoneminder
